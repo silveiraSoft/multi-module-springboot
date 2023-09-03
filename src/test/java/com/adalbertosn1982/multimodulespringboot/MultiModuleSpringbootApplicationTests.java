e314eb53d996fb38ea323fe8c033ab10aceff813
Adalberto Silveira  Napoles <adalbertosn1982@gmail.com>
2023-09-03 14:52:47 -0500

    first commit

---
+++ src/test/java/com/adalbertosn1982/multimodulespringboot/MultiModuleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn1982.multimodulespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
